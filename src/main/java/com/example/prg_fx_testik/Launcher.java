c697125e5dfb309f8d06cffe0f25180a54f48408
drtluk00 <drtluk00@gasos-ro.cz>
2025-12-18 09:07:21 +0100

    >

---
+++ src/main/java/com/example/prg_fx_testik/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.prg_fx_testik;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.prg_fx_testik {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.prg_fx_testik to javafx.fxml;
+    exports com.example.prg_fx_testik;
+}
